627981391d50353006c91f387d4512a595439366
ZiYueCommentary <ziyuecommentary@gmail.com>
2021-08-21 10:44:16 +0800

    修改了矿物词典的错误

--- src/main/java/com/ziyue/diamond/init/ModOreDictionary.java
+++ src/main/java/com/ziyue/diamond/init/ModOreDictionary.java
@@ -4,7 +4,9 @@ import net.minecraftforge.oredict.OreDictionary;
 
 public class ModOreDictionary {
     public static void registerOres(){
+        //干钻石矿
         OreDictionary.registerOre("oreDryDiamond", ModBlocks.DRY_DIAMOND_ORE);
-        OreDictionary.registerOre("ingotDryDiamond", ModItems.DRY_DIAMOND);
+        //干钻石
+        OreDictionary.registerOre("gemDryDiamond", ModItems.DRY_DIAMOND);
     }
 }
